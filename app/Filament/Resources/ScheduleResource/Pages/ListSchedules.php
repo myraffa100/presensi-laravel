c5e2d364a9647f868bc33995736e24c7d54d66d7
muhamadiqbal <myraffa100@github.com>
2025-05-04 09:02:53 +0700

    first commit

---
+++ app/Filament/Resources/ScheduleResource/Pages/ListSchedules.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\ScheduleResource\Pages;
+
+use App\Filament\Resources\ScheduleResource;
+use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Resources\Pages\ListRecords;
+
+class ListSchedules extends ListRecords
+{
+    protected static string $resource = ScheduleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('Presensi')
+                ->url(route('presensi'))
+                ->color('warning'),
+            Actions\CreateAction::make(),
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Presensi;
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('presensi', Presensi::class)->name('presensi');
+});
+
+Route::get('/login', function () {
+    return redirect('admin/login');
+})->name('login');
+
+Route::get('/', function () {
+    return view('welcome');
+});
